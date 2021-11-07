553ab66d7fd0fe8ccbb2c63e027d9b3ea2c0d28f
Serhii-Khrapun <Sergiy.Khrapun@gmail.com>
2021-11-07 19:12:53 +0200

    закінчив 1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let bodyColor = null;
+
+const changeColor = {
+  isActive: false,
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    bodyColor = setInterval(() => {
+      bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  },
+  stop() {
+    clearInterval(bodyColor);
+    this.isActive = false;
+  },
+};
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  changeColor.start();
+});
+stopBtn.addEventListener('click', () => {
+  changeColor.stop();
+});
